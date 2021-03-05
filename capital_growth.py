3a1f6c9c06829c2386832f9df211833f9a3641f9
Syed Mohsin Bukhari <syedmohsinbukhari@googlemail.com>
2021-03-05 21:07:20 +0500

    More elaborate explanation of growth

--- capital_growth.py
+++ capital_growth.py
@@ -7,10 +7,14 @@ def calculate_growth(capital, growth, years):
 
 def calculate_growth_on_consist_invest(capital, investment, growth, years):
     accumulated_capital = calculate_growth(capital, growth, years)
-    for i in range(1, years):
-        accumulated_capital += calculate_growth(investment, growth, i)
+    original_capital = capital
+    for i in range(years - 1, 0, -1):
+        grown_investment = calculate_growth(investment, growth, i)
+        accumulated_capital += grown_investment
+        original_capital += investment
+        print(f"{investment} grew to {grown_investment} in {i} time units")
 
-    return accumulated_capital
+    return accumulated_capital, original_capital
 
 
 def main():
@@ -18,12 +22,13 @@ def main():
     investment = float(sys.argv[2])
     growth = float(sys.argv[3])
     years = int(sys.argv[4])
-    accumulated_capital = calculate_growth_on_consist_invest(
+    accumulated_capital, original_capital = calculate_growth_on_consist_invest(
         capital, investment, growth, years
     )
 
-    capital_growth = accumulated_capital / capital
+    capital_growth = accumulated_capital / original_capital
 
+    print(f"original_capital: {original_capital}")
     print(f"accumulated_capital: {accumulated_capital}")
     print(f"capital_growth: {capital_growth}")
 
